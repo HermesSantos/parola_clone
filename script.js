67bc72dfbb1e2b993368886a2bbb807a093662ef
Hermes Santos <hermessantos995@gmail.com>
2023-01-11 18:17:38 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let palavras = ["bunda"]
-palavras.forEach(palavra => {
-  for(let letra = 0; letra < palavra.length; letra ++){
-    let teste = document.createElement('div')
-    let inputer = document.createElement('input')
-    teste.classList.add('foi')
-    inputer.classList.add('inputer')
-    document.getElementById("saida").appendChild(teste).innerHTML="_"
-    document.getElementById("input").appendChild(inputer)
+let id = 2
+let secretWord = "bunda"
+let givenWord = []
+
+function verifyLetter (letter) {
+  for(let i = 0; i<= 4; i++) {
+    // verifica se a letra e existe
+    secretWord[i] === letter ? console.log("existe") : ""
+    if(secretWord[i] === letter) {
+      // verifica a posição original
+      console.log("posição", secretWord.indexOf(letter))
+    }
   }
-})
+}
+function changeField (value) {
+  if(id >= 6) {
+    id === 6 ? givenWord.push(value) : ''
+    id === 6 ? verifyAnswer() : ''
+  } else {
+    if(value) {
+      document.getElementById(`l${id}`).focus()
+      console.log(id)
+      verifyLetter(value)
+      givenWord.push(value)
+      id++
+    } 
+  }
+}
+function verifyAnswer () {
+  console.log("verificando");
+  let newDiv = document.createElement("div")
+  newDiv.innerHTML = givenWord.join('')
+  let element = document.getElementById("forTheNew")
+  element.appendChild(newDiv)
+  clearFields()
+}
+function clearFields () {
+  for(let i = 1; i<6; i++){
+    let element = document.getElementById(`l${i}`)
+    element.value=""
+  }
+}
